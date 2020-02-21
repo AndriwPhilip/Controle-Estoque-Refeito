62f141a7ff0e7285633b6344a97ff19459cd3b0e
andriwphilip <andriwphilip@gmail.com>
2020-02-21 17:55:12 -0300

    Segurança

---
+++ ControleEstoque.Web/Models/UsuarioModel.cs
@@ -0,0 +1,36 @@
+﻿using ControleEstoque.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Web.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(string login, string senha)
+        {
+            var ret = false;
+            using(var conexao  = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using(var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = "select count(*) from usuario where login=@login and senha=@senha";
+
+                    comando.Parameters.Add("@login", SqlDbType.VarChar).Value = login;
+                    comando.Parameters.Add("@senha", SqlDbType.VarChar).Value = CriptoHelper.HashMD5(senha);
+
+                    ret = ((int)comando.ExecuteScalar() > 0);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
